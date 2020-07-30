0c39f2ac4d63aadc579ca0c18fdb42bacfc4ed38
Marcin Jan Grzybowski <marcinjangrzybowski@gmail.com>
2020-07-30 22:41:20 +0200

    pieces eval WIP

--- src/PiecesEval.hs
+++ src/PiecesEval.hs
@@ -6,7 +6,11 @@ import Syntax
 
 import Combi
 
+import Data.List
+
 import Data.Sort
+
+import Data.Map
 
 
 
@@ -21,17 +25,19 @@ cornerFilter su@(Subset _ ss) s =
           2 -> Set.singleton (i , Set.notMember i ss) 
           _ -> error "imposible!"
             
-  in Set.unions $ map (\i -> f i $ Set.filter (((==) i) . fst ) s) (range n)
+  in Set.unions $ fmap (\i -> f i $ Set.filter (((==) i) . fst ) s) (range n)
 
-permFilter :: Permutation -> Set.Set (Int , Bool) -> Set.Set (Int , Bool)
+permFilter :: Permutation -> Set.Set (Set.Set (Int , Bool)) -> Set.Set (Set.Set (Int , Bool))
 permFilter pm@(Permutation pmm) s =
   let n = sizeLI pm
-        
+      cmp :: Int -> Int -> Ordering
+      cmp x y = compare (pmm ! x) (pmm ! y)         
   in undefined
 
 iExprPE :: Piece -> IExpr -> (Int , Bool)
 iExprPE (su , pm) ie =
   let ie1 = Set.map (Set.filter undefined) ie
+      
   in undefined
 
 piecesEval :: CellExpr -> FromLI Piece PieceExpr
